91252af615d6a1a2e587abf028d85c7866a6c354
nhusaien <nhusaien@outlook.com>
2024-05-23 16:44:20 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('my-app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
